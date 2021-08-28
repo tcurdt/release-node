841794d6f78d112dd86ea77bfe401ba344f4b963
Torsten Curdt <tcurdt@vafer.org>
2021-08-29 00:04:23 +0200

    print version on startup

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const http = require('http')
+const Version = require('./version')
+const Http = require('http')
 
-http.createServer((request, response) => {
+const port = 8080
+
+console.log(`running ${Version.sha} on port ${port}`)
+
+Http.createServer((request, response) => {
   response.writeHead(200, {
       'Content-Type': 'text/plain'
   })
   response.write('Hello, World!\n')
   response.end();
-}).listen(8080);
-
-// Hello.prototype.toString = function() {
-//   return "hello world"
-// }
-
-// module.exports = Hello
+}).listen(port);
